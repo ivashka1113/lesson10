4cc0a9c70d153e1d53454752015d72e74266d752
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-12 21:43:09 +0700

    Выполнил домашнее задание 10

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+
+const books = document.querySelectorAll(".book");
+const ad = document.querySelector(".adv");
+const body = document.querySelector("body");
+const chaptersTwo = books[0].querySelectorAll("li");
+const chaptersFive = books[5].querySelectorAll("li");
+const chaptersSix = books[2].querySelectorAll("li");
+
+
+
+const booksClone = books;
+
+
+books[0].before(books[1]);
+books[2].before(books[4]);
+books[5].after(books[2]);
+
+body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+
+books[4].childNodes[1].childNodes[1].innerHTML = "Книга 3. this и Прототипы Объектов";
+
+ad.remove();
+
+chaptersTwo[3].after(chaptersTwo[6]);
+chaptersTwo[6].after(chaptersTwo[8]);
+chaptersTwo[9].after(chaptersTwo[2]);
+
+chaptersFive[1].after(chaptersFive[9]);
+chaptersFive[9].after(chaptersFive[3]);
+chaptersFive[3].after(chaptersFive[4]);
+chaptersFive[7].after(chaptersFive[5]);
+
+
+chaptersSix[8].insertAdjacentHTML("afterend", "<li><b>Глава 8: За пределами ES6</b></li>")
+console.dir(chaptersSix);
